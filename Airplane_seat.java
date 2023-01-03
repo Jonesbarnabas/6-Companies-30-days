ce95837bf48414d0d572cf7c0ec8502b37380366
Jonesbarnabas <121843816+Jonesbarnabas@users.noreply.github.com>
2023-01-03 11:28:48 +0530

    Day 1

---
+++ Airplane_seat.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Airplane_seat {
+    public static double nthPersonGetsNthSeat(int n) {
+        if(n==1)return 1;
+        double res=1.0/2.0;
+        return res;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(nthPersonGetsNthSeat(n));
+    }
+}
