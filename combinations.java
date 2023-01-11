bb04e2ac9c1bc7ef943d1fe19e7f3703327f0f7a
Jonesbarnabas <121843816+Jonesbarnabas@users.noreply.github.com>
2023-01-11 10:20:28 +0530

    Day 3

---
+++ combinations.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class combinations {
+    public static List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        com(k,n,1,new ArrayList<Integer>(),ans);
+        return ans;
+    }
+    public static void com(int k,int n,int s,List<Integer> path,List<List<Integer>> ans){
+        if(k==path.size() && n==0){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i=s;i<=9;i++){
+            path.add(i);
+            com(k,n-i,i+1,path,ans);
+            path.remove(path.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        System.out.println(combinationSum3(k,n));
+    }
+}
+
